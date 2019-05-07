7da1c9c80f919cce10ab89c5ead2db410630f099
evansavage <esav678@gmail.com>
2019-05-07 14:00:50 -0400

    Comments ordered correctly

--- pdfviewer/views.py
+++ pdfviewer/views.py
@@ -1,20 +1,12 @@
 import datetime
 
 from django.shortcuts import render
-<<<<<<< HEAD
-from django.http import HttpResponse, HttpResponseRedirect
-=======
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
->>>>>>> 2ee6c83771d419334f06298e766424aa6e7a1f48
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from django.urls import reverse
-<<<<<<< HEAD
-=======
-
->>>>>>> 2ee6c83771d419334f06298e766424aa6e7a1f48
 
 # Create your views here.
 from .models import Score, Score_Comments
@@ -28,34 +20,33 @@ def scores(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 def index(request, score_id):
     score = Score.objects.get(pk=score_id)
+    comments = score.score_comments_set.order_by('-date')
     template = loader.get_template('pdfviewer/index.html')
     context = {
         'score': score,
+        'comments': comments
     }
     return HttpResponse(template.render(context, request))
 
+
 def submit(request, score_id):
-    scoreG = Score.objects.get(pk=score_id)
+    scoreG = Score.objects.get(pk=score_id) # get_object_or_404
+    # scoreG.comment_set.create(
+    #     date=
+    #     author=
+    #     text=
+    # )
+
     # template = loader.get_template('pdfviewer/index.html')
-<<<<<<< HEAD
-    comment = Score_Comments.objects.create(date=timezone.now(), score=scoreG)
-    comment.author = request.POST['usrname']
-    comment.text = request.POST['comment']
-    # comment.date = timezone.now()
-    comment.save()
-    context = {}
-    return HttpResponseRedirect(reverse(f'pdfviewer:submit', args=(scoreG.id)))
-    # return HttpResponseRedirect(reverse('pdfviewer:' + score_id), args=(score.id,))
-=======
     comment = Score_Comments.objects.create(date=timezone.now(),score=scoreG)
     #comment.score = score_id
     comment.author = request.POST['usrname']
     comment.text = request.POST['comment']
     #comment.date = timezone.now()
     comment.save()
-    context = {}
+    #context = {}
     #return 1
     return HttpResponseRedirect(reverse('index',args=(scoreG.id,)))
->>>>>>> 2ee6c83771d419334f06298e766424aa6e7a1f48
